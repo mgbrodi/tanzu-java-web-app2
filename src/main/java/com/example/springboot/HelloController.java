90d947829be7d2a92a8d7dcbfe51bcf208c8a7d2
Maria Gabriella Brodi <mbrodi@pivotal.io>
2022-01-26 13:30:04 -0500

    wp

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -12,9 +12,12 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
-		Date date = new Date(System.currentTimeMillis());
-		return formatter.format(date) + "Greetings from Spring Boot + Tanzu!";
+		return "Greetings from Spring Boot + Tanzu!";	
+
+		// SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+		// Date date = new Date(System.currentTimeMillis());
+			
+		//return formatter.format(date) + "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
