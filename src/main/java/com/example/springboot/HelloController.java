de5ded97d8ad5bc0d97610abf973a72726214770
Maria Gabriella Brodi <mbrodi@pivotal.io>
2022-01-26 13:21:21 -0500

    passing tests

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,10 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -8,7 +12,9 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu to S&P global!";
+		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+		Date date = new Date(System.currentTimeMillis());
+		return formatter.format(date) + "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
